a2aff9850f6e2f9ec8472e1e9e8a469ec51ece25
Ayushi Behl <ayushi.behl94@gmail.com>
2024-05-15 16:57:09 -0400

    first commit

---
+++ week2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+    <?php echo ("Hello"); ?>
+    <?php echo '<h1>Welcome to HTTP 5225</h1>' ?>
+    <?php echo '<p>We will learn PHP in this class</p>' ?>
+
+    <h1>Welcome to HTTP 5225</h1>
+    <p>We will learn PHP in this class</p>
+    <p>We will learn the following</p>
+    <ul>
+        <li>PHP</li>
+        <li>SQL</li>
+        <li>Laravel</li>
+    </ul>
+</body>
+</html>
