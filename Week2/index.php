1b1185a470640e18c3f34745f6588f5cdc896de1
Ayushi Behl <ayushi.behl94@gmail.com>
2024-06-12 13:20:18 -0400

    Adding Docker Lab for Workshop

---
+++ Week2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+    <?php
+        
+    
+    ?>
+    <h1>Welcome to HTTP 5225</h1>
+    <p>We will learn PHP in this class</p>
+    <p>We will learn the following</p>
+    <ul>
+        <li>PHP</li>
+    </ul>
+</body>
+</html>
